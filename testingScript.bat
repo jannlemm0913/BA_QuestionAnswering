0367e17936540ee4e4dc966f45436603ea4463b2
Silvan Knecht <knecht.silvan@gmail.com>
2019-03-14 11:58:30 +0100

    Bugfix: skipte

--- testingScript.bat
+++ testingScript.bat
@@ -2,7 +2,7 @@ start cmd /k "cd %STARDOG_HOME% & stardog-admin server start"
 TIMEOUT 5
 start cmd /k "cd %BA_HOME%\BA\Qanary\qanary_pipeline-template\target && java -jar qa.pipeline-1.1.2.jar"
 TIMEOUT 5
-start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-NER-stanford\target && java -jar qa.NER-Standford-1.0.0.jar"
+start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-NER-stanford\target && java -Xmx1G -jar qa.NER-Standford-1.0.0.jar"
 TIMEOUT 5
 start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-NED-AGDISTIS\target && java -jar qa.NED-AGDISTIS-1.0.0.jar"
 TIMEOUT 5
