ced4037afb5c1a46abeaa799d2cb9ef3357ca8aa
Jann Lemm <jann.lemm@bluewin.ch>
2019-04-17 14:06:19 +0200

    scripts 17 to 20 changed to earl from rematch

--- 18_Stanford-NER-AGDISTIS-NED_Earl_CLS-CLISNLIOD_QB.bat
+++ 18_Stanford-NER-AGDISTIS-NED_Earl_CLS-CLISNLIOD_QB.bat
@@ -1,7 +1,7 @@
-echo "Pipeline 17_Stanford-NER-AGDISTIS-NED_Rematch_CLS-CLISNLIOD_SINA"
+echo "Pipeline 18_Stanford-NER-AGDISTIS-NED_Earl_CLS-CLISNLIOD_QB"
 
 start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-NER-stanford\target && java -Xmx1G -jar qa.NER-Standford-1.0.0.jar"
 start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-NED-AGDISTIS\target && java -jar qa.NED-AGDISTIS-1.0.0.jar"
-start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-REL-ReMatch\target && java -jar qanary_component-REL-ReMatch-1.0.0.jar"
+start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qa.qanary_component-Earl-RelationLinking\target && java -jar qa.qanary_component-Earl-RelationLinking-0.0.1.jar"
 start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-CLS-CLSNLIOD\target && java -jar qanary_component-CLS-CLSNLIOD-1.0.0.jar"
-start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qanary_component-QB-Sina\target && java -jar qanary_component-QB-Sina-1.0.0.jar"
+start cmd /k "cd %BA_HOME%\BA\Qanary-question-answering-components\qa.qanary_component-QueryBuilder\target && java -jar qa.qanary_component-QueryBuilder-1.0.2.jar"
